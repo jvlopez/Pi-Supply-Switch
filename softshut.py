2f743d1f3d73937db962959e0b87552ce164c5fa
Jesus Vera <jvlopez@users.noreply.github.com>
2017-01-25 18:52:51 -0600

    Update softshut.py

--- softshut.py
+++ softshut.py
@@ -7,20 +7,17 @@ from time import sleep
 
 # Map pin seven and eight on the Pi Switch PCB to chosen pins on the Raspberry Pi header
 # The PCB numbering is a legacy with the original design of the board
-PinSeven = 7
-PinEight = 11
-GPIO.setmode(GPIO.BOARD) # Set pin numbering to board numbering
-GPIO.setup(PinSeven, GPIO.IN) # Set up PinSeven as an input
-GPIO.setup(PinEight, GPIO.OUT, initial=1) # Setup PinEight as output
+PinFOUR = 7
+GPIO.setmode(GPIO.BCM) # Set pin numbering to board numbering
+GPIO.setup(PinFOUR, GPIO.IN) # Set up PinFOUR as an input
 
-while (GPIO.input(PinSeven) == False): # While button not pressed
- GPIO.wait_for_edge(PinSeven, GPIO.RISING) # Wait for a rising edge on PinSeven
+while (GPIO.input(PinFOUR) == False): # While button not pressed
+ GPIO.wait_for_edge(PinFOUR, GPIO.RISING) # Wait for a rising edge on PinFOUR
  sleep(0.1); # Sleep 100ms to avoid triggering a shutdown when a spike occured
  
 sleep(2); # Sleep 2s to distinguish a long press from a short press
 
-if (GPIO.input(PinSeven) == False):
- GPIO.output(PinEight,0) # Bring down PinEight so that the capacitor can discharge and remove power to the Pi
- call('poweroff', shell=False) # Initiate OS Poweroff
+if (GPIO.input(PinFOUR) == False):
+  call('reboot', shell=False) # Initiate OS Reboot
 else:
- call('reboot', shell=False) # Initiate OS Reboot
+ call('poweroff', shell=False) # Initiate OS Poweroff
